123dd0c5b545fcb33af2a00909d4fd966ee0cd4e
“Leonardo <leo.almeida.seo@gmail.com>
2023-02-13 22:38:30 -0300

    projeto-ctd

---
+++ front-end-ii/aula-01/script.js
@@ -0,0 +1,12 @@
+//Declarando uma variavel
+var userName = 'Leo Almeida' //globais
+let age = 26                 //funções   
+const job = 'Tech'           //constante (não pode ser mudada)
+
+//console.log(userName)
+var userNamePrompt 
+
+userNamePrompt = window.prompt('Olá, qual é o seu nome?')
+console.log('Olá ' + userNamePrompt)
+
+alert('Olá ' + userNamePrompt)
---
+++ front-end-ii/mesa-de-trabalho/aula-01/script.js
@@ -0,0 +1,5 @@
+var nome = window.prompt('Olá, qual o seu nome?')
+var idade = window.prompt('Quantos anos você tem?')
+var profissao = window.prompt('Qual sua profissão?')
+
+alert('Olá me chamo ' + nome + ', tenho ' + idade + ' anos e trabalho com ' + profissao + '.')
